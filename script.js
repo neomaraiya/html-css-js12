6e36918ea19732ad6c471afdf5b6c13190f05305
neomaraiya <neomaraiya@outlook.com>
2021-05-27 17:34:40 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello!')
+console.log('hello!');
+
+const cardID = document.getElementById('cards');
+
+const observer = new IntersectionObserver(entries => {
+  console.log(entries[0].isIntersecting);
+  if (entries[0].isIntersecting) {
+    const image = cardID.children[0];
+    const imageSrc = image.dataset.src;
+    console.log(imageSrc);
+    image.setAttribute('src', imageSrc);
+    return (cardID.style.background = 'red');
+  }
+  return (cardID.style.background = 'green');
+}, {});
+
+observer.observe(cardID);
